92803110b7941869723e17e707f0a76065a9a0c9
nikdelvin <erter.live@gmail.com>
2024-02-26 23:25:03 +0400

    Add Button Group Component

--- dist/index.js
+++ dist/index.js
@@ -1,12 +1,21 @@
 import plugin from 'tailwindcss/plugin'
 import { button } from './components/button'
+import { buttonGroup, buttonGroupButton } from './components/buttonGroup'
+
 const tailyUI = plugin(
   function ({addComponents}) {
     addComponents({
-        '.button': {
-            [button]: {}
-        }
+      '.button': {
+        [button]: {}
+      },
+      '.button-group': {
+        [buttonGroup]: {}
+      },
+      '.button-group .button': {
+        [buttonGroupButton]: {}
+      }
     })
   }
 )
+
 export { tailyUI }
